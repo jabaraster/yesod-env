a1ac94e49400d11ae8eb28a821db6948f4cf99a2
jabaraster <jabaraster@gmail.com>
2014-12-29 13:22:34 +0900

    Update build_yesod_for_ec2.sh

--- build_yesod_for_ec2.sh
+++ build_yesod_for_ec2.sh
@@ -3,8 +3,6 @@
 # 参考URL
 # http://qiita.com/a-suenami/items/ef4266a1b38dd5bd426e
 # 
-# CentOS上でも構築に成功したが、そのコマンドは別の機会に.
-# 
 sudo yum -y update
 sudo yum -y install wget
 sudo yum -y install gcc gmp gmp-devel freeglut freeglut-devel zlib-devel
@@ -32,4 +30,57 @@ cabal install -j --enable-tests --max-backjumps=-1 --reorder-goals --force-reins
 # cabalファイルのコンパイルオプションにTypeSynonymInstancesを追加しないとビルドに失敗する
 # また、２回めのcabal installでは--force-reinstallsオプションは不要
 yesod devel
+
+#################################################
+# PostgreSQLをDBに使う場合の措置
+#################################################
+# PostgreSQL 9.3のインストール
+# CentOS環境下でのサービス名は postgresql93
+sudo rpm -i http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-redhat93-9.3-1.noarch.rpm
+sudo yum -y install postgresql93-server postgresql93-contrib
+sudo service postgresql-9.3 initdb
+sudo chkconfig postgresql-9.3 on
+sudo service postgresql-9.3 start
+
+sudo passwd postgres
+# ここでLinuxのpostgresユーザのパスワードを設定
+
+# ここからはpostgresユーザのpsqlコマンド上で作業
+su - postgres
+psql
+
+# PostgreSQLにアプリ用ユーザを作る.
+create user app createdb password 'app' login;
+
+# ここで一旦psqlから抜け、さらにpostgresユーザからも抜ける.
+# 作成したユーザでpsql接続可能にするために、PostgreSQLの設定ファイルを編集する.
+\q
+exit
+
+# 全てのDB/ユーザでパスワード認証を経ての接続を可能にする
+# CentOSの場合のパスは /var/lib/pgsql/9.3/data/pg_hba.conf
+sudo vi /var/lib/pgsql93/data/pg_hba.conf
+
+(ファイルの内容を下記に置換)
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+# "local" is for Unix domain socket connections only
+local   all             all                                     peer
+
+# IPv4 local connections:
+host    all             postgres        127.0.0.1/32            ident
+host    all             all             0.0.0.0/0               password
+
+# IPv6 local connections:
+host    all             all             ::/0                    password
+
+# 設定ファイルを書き換えた後はPostgreSQLを再起動する.
+sudo service postgresql-9.3 restart
+
+# このままではpersistence-postgresのコンパイルに失敗する.
+# 対処する.
+echo "export PATH=/usr/pgsql-9.3/bin/:\$PATH" >> ~/.bashrc
+source ~/.bashrc
+sudo yum -y install libpqxx-devel
+createdb -U app -h localhost -E UTF8 app
 
